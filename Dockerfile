b6bc7bd13f89c9a027a8e214e6f9f997e1ed6488
alldakr <alldakr@gmail.com>
2024-02-05 23:07:08 +0900

    fixed 4

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,10 @@
-# 베이스 이미지 설정 (예: Python 3.8 이미지 사용)
-FROM python:3.10
+FROM python:3.8-slim
 
-# 작업 디렉토리 설정
-WORKDIR /python-api
+WORKDIR /app
 
-# 현재 디렉토리의 모든 파일을 컨테이너의 /app 디렉토리로 복사
-COPY . /python-api
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
 
-# 필요한 패키지 설치 (requirements.txt 파일이 있다면 사용)
-COPY requirements.txt /python-api/
-RUN pip install --no-cache-dir -r requirements.txt
+COPY . .
 
-# 애플리케이션 실행
-CMD ["python", "main.py"]
+CMD ["python", "app.py"]
---
+++ main copy.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+todos = {}
+
+class TodoResource(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos.get(todo_id, 'Todo not found')}
+
+    def put(self, todo_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('task')
+        args = parser.parse_args()
+        todos[todo_id] = args['task']
+        return {todo_id: todos[todo_id]}
+
+    def delete(self, todo_id):
+        if todo_id in todos:
+            del todos[todo_id]
+            return {'result': 'Todo deleted'}
+        else:
+            return {'result': 'Todo not found'}
+
+class TodoListResource(Resource):
+    def get(self):
+        return todos
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('task')
+        args = parser.parse_args()
+        todo_id = max(map(int, todos.keys())) + 1
+        todos[todo_id] = args['task']
+        return {todo_id: todos[todo_id]}
+
+class Todo(Resource):
+    def get(self):
+        return todos
+
+
+api.add_resource(TodoResource, '/todos/<int:todo_id>')
+api.add_resource(TodoListResource, '/todos')
+api.add_resource(Todo, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
